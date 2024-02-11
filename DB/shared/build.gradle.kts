81cc630050058dddd87717a167e49786659548d8
jjm <mjj0131@gmail.com>
2024-02-10 17:59:47 -0800

    database setup

--- DB/shared/build.gradle.kts
+++ DB/shared/build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    kotlin("plugin.serialization").version("1.9.22")
+    id("com.squareup.sqldelight").version("1.5.5")
 }
 
 kotlin {
@@ -23,10 +25,28 @@ kotlin {
             isStatic = true
         }
     }
-    
+
+    val coroutinesVersion = "1.7.3"
+    val ktorVersion = "2.3.7"
+    val sqlDelightVersion = "1.5.5"
+    val dateTimeVersion = "0.4.1"
+
     sourceSets {
         commonMain.dependencies {
-            // put your Multiplatform dependencies here
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+            implementation("io.ktor:ktor-client-core:$ktorVersion")
+            implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+            implementation("com.squareup.sqldelight:runtime:$sqlDelightVersion")
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:$dateTimeVersion")
+        }
+        androidMain.dependencies {
+            implementation("io.ktor:ktor-client-android:$ktorVersion")
+            implementation("com.squareup.sqldelight:android-driver:$sqlDelightVersion")
+        }
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+            implementation("com.squareup.sqldelight:native-driver:$sqlDelightVersion")
         }
     }
 }
@@ -36,5 +56,11 @@ android {
     compileSdk = libs.versions.android.compileSdk.get().toInt()
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+
+sqldelight {
+    database("AppDatabase") {
+        packageName = "com.jetbrains.handson.kmm.shared.cache"
     }
 }
